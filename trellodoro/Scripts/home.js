7b61ee6c598f66638d1005a53b3b130ab2963de2
Sean Patterson <dillieo@gmail.com>
2014-07-28 23:32:12 -0700

    Added ability to create cards in trellodoro.

--- trellodoro/Scripts/home.js
+++ trellodoro/Scripts/home.js
@@ -19,6 +19,7 @@ $(document).ready(function () {
 	// Hack modal box and time display fill the screen and be centered.
 	$(document).on('opened', '[data-reveal]', function () {
 		$("#status-box").hide();
+		$("#card-status-box").hide();
 		
 		ToggleButton('resume', false);
 		ToggleButton('pause', false);
@@ -49,7 +50,39 @@ $(document).ready(function () {
 	});
 	
 	$('#button-pomodoro').bind('click', function () {
-		addSticker($("#cardSelect").val());
+		addSticker($("#currentCard").attr("data-cardid"));
+	});
+
+	$('#button-cancel-card').bind('click', function() {
+		$('#card-modal').foundation('reveal', 'close');
+	});
+
+	$('#button-create-card').bind('click', function() {
+		var listId = $("#newCardListSelect option:selected").val();
+		var cardName = $("#newCardName").val();
+		var myId = $('#trelloLegend').attr('data-memberid');
+
+		Trello.post("cards/",
+			{ idList: listId, name: cardName, idMembers: myId },
+			function (result) {
+				$("#newCardName").val('');
+				$("#card-status-box").show(300).delay(800).fadeOut(400);
+			});
+	});
+	
+	$('#button-create-launch-card').bind('click', function () {
+		var listId = $("#newCardListSelect option:selected").val();
+		var cardName = $("#newCardName").val();
+		var myId = $('#trelloLegend').attr('data-memberid');
+
+		Trello.post("cards/",
+			{ idList: listId, name: cardName, idMembers: myId },
+			function (result) {
+				$("#currentCard").text($("#newCardName").val());
+				$("#currentCard").attr("data-cardid", result.id);
+				$("#newCardName").val('');				
+				$('#clock-modal').foundation('reveal', 'open');
+			});
 	});
 	
 	// Wire up Trello functionality
@@ -58,6 +91,7 @@ $(document).ready(function () {
 
 		Trello.members.get("me", function (member) {
 			$('#trelloLegend').text("Trello (Logged in as " + member.fullName + ")");
+			$('#trelloLegend').attr("data-memberid", member.id);
 			buildBoardList();
 		});
 	};
@@ -101,11 +135,13 @@ $(document).ready(function () {
 
 	$("#disconnectTrello").click(logout);
 
-	$("#btn-refresh-cards").click(buildBoardList);
+	$("#button-refresh-cards").click(buildBoardList);
 
 	$("#cardSelect").change(function() {
 		var cardName = $("#cardSelect option:selected").text();
+		var cardId = $("#cardSelect option:selected").val();
 
+		$("#currentCard").attr("data-cardid", cardId);
 		$("#currentCard").text(cardName);
 	});
 	
@@ -127,6 +163,26 @@ $(document).ready(function () {
 			});		
 
 		$('#cardSelect').show();
+	});
+	
+	$("#newCardBoardSelect").change(function () {
+		var boardId = $("#newCardBoardSelect option:selected").val();
+
+		$('#newCardListSelect').empty();
+
+		// Append "Select a list" for functionality.
+		$('#newCardListSelect').append('<option value="-99">Select a list...</option>');
+
+		Trello.get("boards/" + boardId + "/lists",
+			function (cards) {
+
+				// Build select list with card details.
+				$.each(cards, function (ix, list) {
+					$('#newCardListSelect').append('<option value="' + list.id + '">' + list.name + '</option>');
+				});
+			});
+
+		$('#newCardListSelect').show();
 	});
 	
 	function addSticker(cardId)
@@ -151,6 +207,8 @@ $(document).ready(function () {
 		Trello.get("members/me/boards", function (boards) {
 			$('#cardSelect').empty();
 			$('#boardSelect').empty();
+			$('#newCardBoardSelect').empty();
+			$('#newCardListSelect').empty();
 
 			// Build select list with board details.
 			$.each(boards, function (ix, board) {
@@ -158,11 +216,13 @@ $(document).ready(function () {
 				// sort alphabetically.
 				if (firstBoard) {
 					$('#boardSelect').append('<option value="-99">Select a board...</option>');
+					$('#newCardBoardSelect').append('<option value="-99">Select a board...</option>');
 					$('#cardSelect').append('<option value="-99">Select a card...</option>');
 					firstBoard = false;
 				}
 				
-				$('#boardSelect').append('<option value="' + board.id + '">' + board.name + '</option>');			
+				$('#boardSelect').append('<option value="' + board.id + '">' + board.name + '</option>');
+				$('#newCardBoardSelect').append('<option value="' + board.id + '">' + board.name + '</option>');
 			});
 		});
 	}
